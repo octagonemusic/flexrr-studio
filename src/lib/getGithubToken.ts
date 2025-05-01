ceb6db865b689a40efde32915c0706bcf8d8bc10
octagonemusic <octagonemusic69@gmail.com>
2025-05-01 13:17:54 +0530

    Fixed API endpoints

--- src/lib/getGithubToken.ts
+++ src/lib/getGithubToken.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 import { User } from "@/models/User";
 
 export async function getGithubAccessToken() {
